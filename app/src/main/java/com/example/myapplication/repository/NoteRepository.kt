cdc74ff057e5e4ab1390ecaa95e787a11cd0cd6a
Omar <omarpk148@gmail.com>
2024-03-07 22:10:35 -0500

    NoteRepository, navigation:argument

---
+++ app/src/main/java/com/example/myapplication/repository/NoteRepository.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication.repository
+
+import androidx.room.Query
+import com.example.myapplication.database.NoteDatabase
+import com.example.myapplication.model.Note
+
+class NoteRepository(private val db: NoteDatabase) {
+
+    suspend fun  insertNote(note: Note) = db.getNOteDao().insertNote(note)
+    suspend fun  deleteNote(note: Note) = db.getNOteDao().deleteNote(note)
+    suspend fun  updateNote(note: Note) = db.getNOteDao().updateNote(note)
+
+    fun getAllNotes() = db.getNOteDao().getALLNotes()
+    fun searchNotes(query: String) =db.getNOteDao().searchNote(query)
+}
